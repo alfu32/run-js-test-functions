c9e8ebb62a015fa55b0a2e77d2ba45780e597415
alfu32 <ioan.alferaru@gmail.com>
2025-07-14 02:25:44 +0200

    fix(runner)

--- webpack.config.js
+++ webpack.config.js
@@ -46,10 +46,10 @@ const extensionConfig = {
     level: "log", // enables logging required for problem matchers
   },
   plugins: [
-    // <-- copy runner.js from src (or your project root) into out/
+    // <-- copy runner.mjs from src (or your project root) into out/
     new CopyPlugin({
       patterns: [
-        { from: 'src/runner.js', to: 'runner.js' }
+        { from: 'src/runner.mjs', to: 'runner.mjs' }
       ]
     })
   ]
